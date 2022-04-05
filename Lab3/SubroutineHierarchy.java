76ce1395b7b7482bba1ffe0bb6a5376f379841cd
Krzysztof Walusiak <58446367+kurh234@users.noreply.github.com>
2022-04-05 09:15:26 +0200

    Update SubroutineHierarchy.java

--- Lab3/SubroutineHierarchy.java
+++ Lab3/SubroutineHierarchy.java
@@ -2,7 +2,6 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.awt.geom.*;
-//import java.util.ArrayList;
 
 /**
  * A panel that displays a two-dimensional animation that is drawn
